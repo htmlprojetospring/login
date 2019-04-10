dfa70ed84a9e2477e0ba563e1514fa3ad174d474
Alvaro Pereira do Nascimento <33030108+AlvaroPereiradoNascimento@users.noreply.github.com>
2019-04-10 18:13:43 -0300

    Exportado corretamente o javascript para arquivo
    Funcionalidade ainda continuam

--- javascript/main.js
+++ javascript/main.js
@@ -3,15 +3,13 @@
     // add ES6 poly-fill for the Promise, if needed (or rewrite to use a callback)	  
     // add ES6 poly-fill for the Promise, if needed (or rewrite to use a callback)
     // função que efetua troca dinâmica da folha de estilo
-  function addcss(css){	  function addcss(css){
+  function addcss(css){ 
     var head = document.getElementsByTagName('head')[0];
-    var head = document.getElementsByTagName('head')[0];
-    head.removeChild(head.childNodes[8])	    head.removeChild(head.childNodes[8])
+    head.removeChild(head.childNodes[8])	    
     var s = document.createElement('link');	 
-    var s = document.createElement('link');
-    s.rel  = 'stylesheet';	    s.rel  = 'stylesheet';
-    s.type = 'text/css';	    s.type = 'text/css';
-    s.href = css;	    s.href = css;
-    s.media = 'all';	    s.media = 'all';
-    head.appendChild(s);	    head.appendChild(s);
+    s.rel  = 'stylesheet';	    
+    s.type = 'text/css';
+    s.href = css;
+    s.media = 'all';
+    head.appendChild(s);
  }
