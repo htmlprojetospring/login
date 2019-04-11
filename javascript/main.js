7f021b5bd5c05deb5cd932632cca3a3717995362
Alvaro Pereira do Nascimento <33030108+AlvaroPereiradoNascimento@users.noreply.github.com>
2019-04-11 01:41:51 -0300

    efetuado mudança do layout do sistema.

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,8 @@
-    // this will work in IE 10, 11 and Safari/Chrome/Firefox/Edge	   
-    // this will work in IE 10, 11 and Safari/Chrome/Firefox/Edge
-    // add ES6 poly-fill for the Promise, if needed (or rewrite to use a callback)	  
-    // add ES6 poly-fill for the Promise, if needed (or rewrite to use a callback)
-    // função que efetua troca dinâmica da folha de estilo
-  function addcss(css){ 
+    function addcss(css){
     var head = document.getElementsByTagName('head')[0];
-    head.removeChild(head.childNodes[8])	    
-    var s = document.createElement('link');	 
-    s.rel  = 'stylesheet';	    
+    head.removeChild(head.childNodes[8])
+    var s = document.createElement('link');
+    s.rel  = 'stylesheet';
     s.type = 'text/css';
     s.href = css;
     s.media = 'all';
