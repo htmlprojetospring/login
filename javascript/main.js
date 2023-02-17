a580eeeb5390467cd5ff4532a6c3169f08f488f8
Alvaro Pereira do Nascimento <alvaropereira15@gmail.com>
2023-02-17 14:28:00 -0300

    melhorias no processos de exibição dos elementos.

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,18 @@
-    function addcss(css){
+function addcss(css){
     var head = document.getElementsByTagName('head')[0];
-    head.removeChild(head.childNodes[8])
+    const elem = document.getElementById('fancy');
+    
+    elem.disabled = true;
+
+    head.removeChild(elem);
+
     var s = document.createElement('link');
     s.rel  = 'stylesheet';
     s.type = 'text/css';
     s.href = css;
     s.media = 'all';
+    s.id = 'fancy';
+    
     head.appendChild(s);
- }
+
+}
